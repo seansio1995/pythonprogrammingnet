cf2672f5dcb1c12ab7936a93fe0cd9506126858e
seansio1995 <seansio@163.com>
2017-09-22 23:10:48 -0400

    add visualization of result

--- imageRecognition/numberTest.py
+++ imageRecognition/numberTest.py
@@ -2,7 +2,7 @@ __author__="Chufan Xiao"
 
 from PIL import Image
 import numpy as np
-
+import matplotlib.pyplot as plt
 import time
 from collections import Counter
 def createExamples():
@@ -58,7 +58,25 @@ def recognizeNumber(filepath):
 
     print(match)
     print(Counter(match))
+    counter=Counter(match)
+    graphX=[]
+    graphY=[]
+    for item in counter:
+        graphX.append(int(item))
+        graphY.append(counter[item])
     result=Counter(match).most_common(1)[0][0]
+    fig=plt.figure()
+    ax1 = plt.subplot2grid((4,4),(0,0), rowspan=1, colspan=4)
+    ax2 = plt.subplot2grid((4,4),(1,0), rowspan=3,colspan=4)
+
+    ax1.imshow(testImageArr)
+    print(graphX)
+    print(graphY)
+    ax2.bar(graphX,graphY,align="center")
+    plt.ylim(400)
+    xloc = plt.MaxNLocator(12)
+    ax2.xaxis.set_major_locator(xloc)
+    plt.show()
     print(result)
 
 
